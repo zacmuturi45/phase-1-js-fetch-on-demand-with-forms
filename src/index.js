232c4295294125165fa9fd3b125d6412c99862d7
isaacgichuhi <zacmuturi45@gmail.com>
2023-10-04 22:54:44 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
+
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.querySelector('input#searchByID');
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(resp => resp.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+
+            title.textContent = data.title;
+            summary.textContent = data.summary;
+        })
+    })
   
 }
 
